ad4089e62f6d6e64077e6892c9f2a9f5c2fd435f
MalakaSenanayake <contact.malaka.senanayake@gmail.com>
2025-11-07 05:38:32 +0530

    Removed PropertyReader.initialize method's Content.

--- src/main/java/com/property/Print.java
+++ src/main/java/com/property/Print.java
@@ -7,22 +7,25 @@ class Print {
     private static final String YELLOW = "\u001B[33m";
     private static final String BLUE = "\u001B[34m";
 
+    //------------------------------------------------------------------------------------------------------------------
     public static void info(String message) {
         System.out.println("[" + BLUE + "INFO" + RESET + "] " + message);
     }
-
+    //------------------------------------------------------------------------------------------------------------------
     public static void error(String message) {
         System.out.println("[" + RED + "ERROR" + RESET + "] " + message);
     }
-
+    //------------------------------------------------------------------------------------------------------------------
     public static void warning(String message) {
         System.out.println("[" + YELLOW + "WARNING" + RESET + "] " + message);
     }
-
+    //------------------------------------------------------------------------------------------------------------------
     public static void msgGreen(String message) {
         System.out.println("[" + BLUE + "INFO" + RESET + "] " + GREEN+message+RESET);
     }
+    //------------------------------------------------------------------------------------------------------------------
     public static void msgBlue(String message) {
         System.out.println("[" + BLUE + "INFO" + RESET + "] " + BLUE+message+RESET);
     }
+    //------------------------------------------------------------------------------------------------------------------
 }
