ac2721fc7ba6ca13919ecfa976269049e15fa4cb
MalakaSenanayake <contact.malaka.senanayake@gmail.com>
2025-12-24 20:12:37 +0530

    Commented the test class

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,6 +2,6 @@ import com.mavora.property.PropertyReader;
 
 public class Test {
     public static void main(String[] args) {
-        PropertyReader.initialize();
+       // PropertyReader.initialize();
     }
 }
